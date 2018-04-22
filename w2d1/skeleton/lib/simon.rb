5afc80e46062495553203b09d1d18ea6ab93ce2f
doahuang <doahuang@gmail.com>
2018-04-22 02:52:20 -0700

    Updated playable simon game

--- w2d1/skeleton/lib/simon.rb
+++ w2d1/skeleton/lib/simon.rb
@@ -10,9 +10,8 @@ class Simon
   end
 
   def play
-    until game_over
-      take_turn
-    end
+    welcome
+    take_turn until game_over
     game_over_message
     reset_game
   end
@@ -20,16 +19,29 @@ class Simon
   def take_turn
     show_sequence
     require_sequence
-    round_success_message
+    round_success_message unless game_over
     self.sequence_length += 1
   end
 
   def show_sequence
     add_random_color
+    seq.each do |color|
+      clear_screen
+      puts "#{color}"
+      sleep(0.5)
+      clear_screen
+      sleep(0.5)
+    end
   end
 
   def require_sequence
-    
+    puts "enter each color in order\n\n"
+    seq.each do |color|
+      unless match?(gets.chomp, color)
+        self.game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -37,11 +49,12 @@ class Simon
   end
 
   def round_success_message
-    puts "round success!"
+    puts "\nnext round..."
+    sleep(1)
   end
 
   def game_over_message
-    puts "game over!"
+    puts "\ngame over!\nyou made it to round #{sequence_length - 1}"
   end
 
   def reset_game
@@ -49,4 +62,21 @@ class Simon
     self.game_over = false
     self.seq = []
   end
+
+  private
+  def welcome
+    clear_screen
+    puts "let's play Simon"
+    gets.chomp
+  end
+
+  def match?(color, other_color)
+    color.size > 1 ? color == other_color : color == other_color[0]
+  end
+
+  def clear_screen
+    system("clear")
+  end
 end
+
+Simon.new.play if $PROGRAM_NAME == __FILE__
