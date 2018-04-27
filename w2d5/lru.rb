0d9da3863d8f829b8c411b2be5113905afb2a600
doahuang <doahuang@gmail.com>
2018-04-27 00:55:34 -0700

    Uploaded w2d5 homework

--- w2d5/lru.rb
+++ w2d5/lru.rb
@@ -1,20 +1,57 @@
 class LRUCache
-    def initialize
-    end
-
-    def count
-      # returns number of elements currently in cache
-    end
-
-    def add(el)
-      # adds element to cache according to LRU principle
-    end
-
-    def show
-      # shows the items in the cache, with the LRU item first
-    end
-
-    private
-    # helper methods go here!
-
+  def initialize(cache_size)
+    @cache_size = cache_size
+    @cache = {}
   end
+
+  def count
+    cache.count
+  end
+
+  def add(el)
+    reset_recent
+    delete_oldest if count == cache_size
+    cache[el] = [1, 1]
+    el
+  end
+
+  def show
+    sort.map{ |key, _| key }
+  end
+
+  def inspect
+    show
+  end
+
+  private
+  attr_reader :cache_size, :cache
+
+  def delete_oldest
+    cache.delete(show.first)
+  end
+
+  def sort
+    cache.sort_by{ |_, (freq, recent)| freq + recent }
+  end
+
+  def reset_recent
+    cache.each{ |key, (_, recent)| cache[key]= [_, 0] }
+  end
+end
+
+
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+p johnny_cache.show
