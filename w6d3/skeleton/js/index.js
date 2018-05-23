1d20ca0f2e613545910c82e84cf4f8e78baa6eeb
doahuang <doahuang@gmail.com>
2018-05-22 22:06:53 -0700

    Updated w6d3 homework

--- w6d3/skeleton/js/index.js
+++ w6d3/skeleton/js/index.js
@@ -5,11 +5,12 @@ $.ajax({
   type: 'GET',
   url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
 
-  success: function(data) {
+  success(data) {
     console.log(data);
+    $('body').append('return result: <br><br>');
     $('body').append(JSON.stringify(data));
   },
-  error: function(err) {
+  error(err) {
     console.log(err);
   }
 });
