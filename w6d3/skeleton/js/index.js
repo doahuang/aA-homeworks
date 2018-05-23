989c49cae486f06339b9dc7f12fbbf2e6614826d
doahuang <doahuang@gmail.com>
2018-05-22 22:03:41 -0700

    Uploaded w6d3 homework

---
+++ w6d3/skeleton/js/index.js
@@ -0,0 +1,17 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+
+  success: function(data) {
+    console.log(data);
+    $('body').append(JSON.stringify(data));
+  },
+  error: function(err) {
+    console.log(err);
+  }
+});
+// Add another console log here, outside your AJAX request
+console.log("AJAX not bad");
