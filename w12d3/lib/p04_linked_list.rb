9d8d7ee7cd2418a3cc4c95aa5d8f6c6958fb34b0
doahuang <doahuang@gmail.com>
2018-07-03 23:42:43 -0700

    Uploaded w12d3

---
+++ w12d3/lib/p04_linked_list.rb
@@ -0,0 +1,100 @@
+class Node
+  attr_accessor :key, :val, :next, :prev
+
+  def initialize(key = nil, val = nil)
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@key}: #{@val}"
+  end
+
+  def remove
+    # optional but useful, connects previous node to next node
+    # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+    @next = nil
+    @prev = nil
+    self
+  end
+end
+
+class LinkedList
+  include Enumerable
+
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
+  end
+
+  def [](i)
+    each_with_index { |node, j| return node if i == j }
+    nil
+  end
+
+  def first
+    @head.next
+  end
+
+  def last
+    @tail.prev
+  end
+
+  def empty?
+    @head.next == @tail
+  end
+
+  def get(key)
+    each{ |node| return node.val if node.key == key }
+  end
+
+  def include?(key)
+    each{ |node| return true if node.key == key }
+    return false;
+  end
+
+  def append(key, val)
+    node = Node.new(key, val)
+    node.next = @tail
+    node.prev = @tail.prev
+    @tail.prev = node
+    node.prev.next = node
+  end
+
+  def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
+  end
+
+  def remove(key)
+    each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
+  end
+
+  def each
+    current_node = @head.next
+    while current_node.next
+      yield current_node
+      current_node = current_node.next
+    end
+  end
+
+  # uncomment when you have `each` working and `Enumerable` included
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
+end
---
+++ w12d3/lib/p06_lru_cache.rb
@@ -0,0 +1,55 @@
+require_relative 'p05_hash_map'
+require_relative 'p04_linked_list'
+
+class LRUCache
+  attr_reader :count
+  def initialize(max, prc)
+    @map = HashMap.new
+    @store = LinkedList.new
+    @max = max
+    @prc = prc
+  end
+
+  def count
+    map.count
+  end
+
+  def get(key)
+    node = map[key]
+    if node
+      update_node!(node)
+      node.val
+    else 
+      calc!(key)
+    end
+  end
+
+  def to_s
+    "Map: " + @map.to_s + "\n" + "Store: " + @store.to_s
+  end
+
+  private
+
+  attr_reader :store, :map, :max
+
+  def calc!(key)
+    # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    node = store.append(key, val)
+    map[key] = node
+    eject! if count > max
+    val
+  end
+
+  def update_node!(node)
+    # suggested helper method; move a node to the end of the list
+    node.remove
+    map[node.key] = store.append(node.key, node.val)
+  end
+
+  def eject!
+    node = store.first
+    node.remove
+    map.delete(node.key)
+  end
+end
