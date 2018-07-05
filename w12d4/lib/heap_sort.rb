233df91a143bbb5489a92ecc96f7a1702d95d477
doahuang <doahuang@gmail.com>
2018-07-05 00:15:56 -0700

    Fixed bugs

--- w12d4/lib/heap_sort.rb
+++ w12d4/lib/heap_sort.rb
@@ -2,8 +2,8 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    (2..length).each do |i|
-      BinaryMinHeap.heapify_up(self, i - 1)
+    (1..length).each do |i|
+      BinaryMinHeap.heapify_up(self, i)
     end
     length.downto(2).each do |i|
       self[0], self[i - 1] = self[i - 1], self[0]
