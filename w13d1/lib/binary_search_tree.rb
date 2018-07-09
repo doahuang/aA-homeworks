1d9a6158e9b2a557bf85da34a38db5d83ebe938d
doahuang <doahuang@gmail.com>
2018-07-08 23:06:21 -0700

    Uploaded w13d1

--- w13d1/lib/binary_search_tree.rb
+++ w13d1/lib/binary_search_tree.rb
@@ -9,18 +9,38 @@ class BinarySearchTree
   end
 
   def insert(value)
-    root ? self.class.insert(value, root) : @root = BSTNode.new(value)
+    if root
+      rec_insert(value)
+    else
+      @root = BSTNode.new(value)
+    end
   end
 
   def find(value, tree_node = @root)
     return nil unless tree_node
     return tree_node if value == tree_node.value
-    tree_node = value < tree_node.value ? tree_node.left : tree_node.right
+    tree_node = value < tree_node.value ? tree_node.left : tree_node.right 
     find(value, tree_node)
   end
 
-  def delete(value)
-    
+  def delete(value, node = @root)
+    return nil unless node
+    if value == node.value
+      return @root = nil unless node.parent
+      return node.left unless node.right
+      return node.right unless node.left
+      new_node = maximum(node.left)
+      new_node.parent.right = new_node.left
+      new_node.left = node.left
+      new_node.right = node.right
+      return new_node
+    end
+    if value < node.value
+      node.left = delete(value, node.left)
+    else
+      node.right = delete(value, node.right)
+    end
+    node
   end
 
   # helper method for #delete:
@@ -50,13 +70,13 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
-  def self.insert(value, node)
+  def rec_insert(value, node = @root)
     return BSTNode.new(value) unless node
     if value <= node.value
-      node.left = insert(value, node.left)
+      node.left = rec_insert(value, node.left)
       node.left.parent = node
     else
-      node.right = insert(value, node.right)
+      node.right = rec_insert(value, node.right)
       node.right.parent = node
     end
     node
--- w13d1/lib/bst_node.rb
+++ w13d1/lib/bst_node.rb
@@ -2,8 +2,6 @@ class BSTNode
   attr_accessor :value, :left, :right, :parent
   def initialize(value)
     @value = value
-    @left = nil
-    @right = nil
-    @parent = nil
+    @left, @right, @parent = nil
   end
 end
