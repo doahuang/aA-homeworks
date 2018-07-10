c08b9073de14c57f65692fcbe13a442fd8770624
doahuang <doahuang@gmail.com>
2018-07-10 14:45:09 -0700

    Updated w13d2

--- w13d2/lib/topological_sort.rb
+++ w13d2/lib/topological_sort.rb
@@ -2,47 +2,55 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
-def topological_sort(vertices)
-  sorted = []
-  queue = []
-
-  vertices.each do |vertex|
-    queue << vertex if vertex.in_edges.empty?
-  end
-  
-  until queue.empty?
-    current = queue.pop
-    sorted << current
-    current.out_edges.each do |edge|
-      dest = edge.to_vertex
-      dest.in_edges.delete(edge)
-      queue << dest if dest.in_edges.empty?
-    end
-  end
-  
-  sorted.length == vertices.length ? sorted : []
-end
-
 # def topological_sort(vertices)
 #   sorted = []
-#   visited = [] 
+#   queue = []
 
 #   vertices.each do |vertex|
-#     visit(vertex, visited, sorted)
+#     queue << vertex if vertex.in_edges.empty?
 #   end
-
-#   sorted
+  
+#   until queue.empty?
+#     current = queue.pop
+#     sorted << current
+#     current.out_edges.each do |edge|
+#       dest = edge.to_vertex
+#       dest.in_edges.delete(edge)
+#       queue << dest if dest.in_edges.empty?
+#     end
+#   end
+  
+#   sorted.length == vertices.length ? sorted : []
 # end
 
-# def visit(vertex, visited, sorted)
-#   return if visited.include?(vertex)
-#   visited << vertex 
+def topological_sort(vertices)
+  sorted = []
+  visited = Set.new
+  temp = Set.new
+  cycle = false
 
-#   vertex.out_edges.each do |edge|
-#     dest = edge.to_vertex
-#     visit(dest, visited, sorted)
-#   end
+  vertices.each do |vertex|
+    cycle = visit(vertex, visited, sorted, temp, cycle)
+    return [] if cycle
+  end
 
-#   sorted.unshift(vertex)
-# end
+  sorted
+end
+
+def visit(vertex, visited, sorted, temp, cycle)
+  return if visited.include?(vertex)
+  return true if temp.include?(vertex)
+  temp.add(vertex)
+  
+  vertex.out_edges.each do |edge|
+    dest = edge.to_vertex
+    cycle = visit(dest, visited, sorted, temp, cycle)
+    return true if cycle
+  end
+  
+  visited.add(vertex)
+  temp.delete(vertex)
+  sorted.unshift(vertex)
+  false
+end
   
