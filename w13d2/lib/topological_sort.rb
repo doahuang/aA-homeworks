04e119bc56507dddeea222d02aaa4e7680515806
doahuang <doahuang@gmail.com>
2018-07-09 23:00:19 -0700

    Finished graph, passed topo sort first spec

---
+++ w13d2/lib/topological_sort.rb
@@ -0,0 +1,24 @@
+require_relative 'graph'
+
+# Implementing topological sort using both Khan's and Tarian's algorithms
+
+def topological_sort(vertices)
+  sorted = []
+  queue = []
+
+  vertices.each do |vertex|
+    queue << vertex if vertex.in_edges.empty?
+  end
+
+  until queue.empty?
+    current = queue.pop
+    sorted << current
+    current.out_edges.each do |edge|
+      dest = edge.to_vertex
+      dest.in_edges.delete(edge)
+      queue << dest if dest.in_edges.empty?
+    end
+  end
+  
+  sorted
+end
