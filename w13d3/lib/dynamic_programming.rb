45f21ee91d07a68e84d7cd78b1a30291618fa1f4
doahuang <doahuang@gmail.com>
2018-07-10 22:57:33 -0700

    Finished blair nums

--- w13d3/lib/dynamic_programming.rb
+++ w13d3/lib/dynamic_programming.rb
@@ -1,11 +1,14 @@
 class DynamicProgramming
-
+  attr_reader :blair_cache
   def initialize
-
+    @blair_cache = { 1 => 1, 2 => 2 }
   end
 
   def blair_nums(n)
-
+    return blair_cache[n] if blair_cache[n]
+    res = blair_nums(n - 2) + blair_nums(n - 1) + 2 * n - 3
+    blair_cache[n] = res
+    res
   end
 
   def frog_hops_bottom_up(n)
