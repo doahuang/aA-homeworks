496ffb884a0cf1c4dc161d1b6e1d74ebc51b4a17
doahuang <doahuang@gmail.com>
2018-07-11 00:24:10 -0700

    Finished frog hop top down

--- w13d3/lib/dynamic_programming.rb
+++ w13d3/lib/dynamic_programming.rb
@@ -1,15 +1,19 @@
 class DynamicProgramming
-  attr_reader :blair_cache
+  attr_reader :blair_cache, :frog_cache
+
   def initialize
     @blair_cache = { 1 => 1, 2 => 2 }
-    @frog_cache = {}
+    @frog_cache = {
+      1 => [[1]], 
+      2 => [[1, 1], [2]],
+      3 => [[1, 1, 1], [1, 2], [2, 1], [3]]
+    }
   end
 
   def blair_nums(n)
     return blair_cache[n] if blair_cache[n]
     res = blair_nums(n - 2) + blair_nums(n - 1) + 2 * n - 3
     blair_cache[n] = res
-    res
   end
 
   def frog_hops_bottom_up(n)
@@ -18,30 +22,32 @@ class DynamicProgramming
   end
 
   def frog_cache_builder(n)
-    cache = { 
-      1 => [[1]], 
-      2 => [[1, 1], [2]],
-      3 => [[1, 1, 1], [1, 2], [2, 1], [3]]
-    }
-    return cache if n < 4
+    return frog_cache if n < 4
     (4..n).each do |stair|
       res = []
-      (1..3).each do |jump|
-        cache[stair - jump].each do |arr|
-          res << arr + [jump]
+      (1..3).each do |i|
+        frog_cache[stair - i].each do |arr|
+          res << arr + [i]
         end
       end
-      cache[stair] = res
+      frog_cache[stair] = res
     end
-    cache
+    frog_cache
   end
 
   def frog_hops_top_down(n)
-
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
-
+    return frog_cache[n] if frog_cache[n]
+    res = []
+    (1..3).each do |i|
+      frog_hops_top_down_helper(n - i).each do |arr|
+        res << arr + [i]
+      end
+    end
+    frog_cache[n] = res
   end
 
   def super_frog_hops(n, k)
